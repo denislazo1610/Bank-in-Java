774d626308323429b35ef3a4181322864c2531f9
denislazo1610 <77086455+denislazo1610@users.noreply.github.com>
2021-02-16 20:52:45 -0700

    changes

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main (String[] args){
         char choice;
         HashMap<String, String> accounts = new HashMap<String, String>();
         HashMap<String, Double> dinero = new HashMap<String, Double>();
@@ -25,7 +26,7 @@ public class Main {
 
     public static void action(char choice, HashMap <String, String> accounts, HashMap<String, Double> dinero){        
         if ((choice == 'a') || (choice == 'A')) {
-
+            cls();
             System.out.println("Create an account");
             optionA(accounts, dinero);
             option();
@@ -37,6 +38,7 @@ public class Main {
             action(choice, accounts, dinero);
 
         }else if ((choice == 'b') || (choice == 'B')) {
+            cls();
             System.out.println("Opening an account");
             optionB(accounts, dinero);
             option();
@@ -48,13 +50,14 @@ public class Main {
             action(choice, accounts, dinero);
 
         }else if ((choice == 'c') || (choice == 'C')){
+            cls();
             System.out.println("Exit, Thank You!");
-
 
         }
     }
 
     public static void optionA(HashMap <String, String> accounts, HashMap<String, Double> dinero){
+        cls();
         System.out.println("You choose option A");
         String username;
         String password;
@@ -72,11 +75,11 @@ public class Main {
         plata = amount.nextDouble();
         accounts.put(username, password);
         dinero.put(username, plata);
-
-        System.out.println("Option A is being selected");
+        cls();
     }
 
     public static void optionB(HashMap <String, String> accounts, HashMap<String, Double> dinero){
+        cls();
         System.out.println("Option B is being selected!");
         System.out.println("These are all the accounts");
         System.out.println("");
@@ -105,6 +108,7 @@ public class Main {
         System.out.println("This is comprobar " + comprobar);
 
         if (password.equals(comprobar)){
+            System.out.println("");
             System.out.println("Opening account....");
             openingAccount(cuenta, accounts, dinero);
         }else {
@@ -115,6 +119,7 @@ public class Main {
     }
 
     public static void openingAccount(String account,HashMap <String, String> accounts, HashMap<String, Double> dinero){
+        cls();
         System.out.println("Welcome " + account);
         char choice;
         System.out.println("");
@@ -130,24 +135,32 @@ public class Main {
         choice = Character.toUpperCase(choice);
 
         if (choice == 'A'){
+            cls();
             System.out.println("Looking money");
             Double plata = dinero.get(account);
             System.out.println("This is how much you have: " + plata);
+            System.out.println("Is this information correct? ");
+            String answer;
+            Scanner decidir = new Scanner(System.in);
+            answer = decidir.nextLine();
             openingAccount(account, accounts, dinero);
         }else if(choice == 'B'){
-            System.out.println("What do you want to do? Depositing or retiring money ?");
+            cls();
+            System.out.println("What do you want to do? Deposit or retire money ?");
             String desicion;
             Scanner decidir = new Scanner(System.in);
             desicion = decidir.nextLine();
 
-            if((desicion.equals("Depositing")) || (desicion.equals("depositing")) ){
+            if((desicion.equals("Deposit")) || (desicion.equals("deposit")) ){
+                cls();
                 System.out.println("How much are you going to deposit? ");
                 double deposito;
                 Scanner respuesta = new Scanner(System.in);
                 deposito = respuesta.nextDouble();
                 dinero.put(account, dinero.get(account) + deposito);
 
-            }else if ((desicion.equals("Retiring")) || (desicion.equals("retiring"))){
+            }else if ((desicion.equals("Retire")) || (desicion.equals("retire"))){
+                cls();
                 System.out.println("How much are you going to retire? ");
                 double retirar;
                 Scanner respuesta = new Scanner(System.in);
@@ -157,6 +170,7 @@ public class Main {
 
             openingAccount(account, accounts, dinero);
         }else if (choice == 'C'){
+            cls();
             System.out.println("Changing unername");
             System.out.println("What do you want to replace your username? ");
             String answer;
@@ -171,7 +185,8 @@ public class Main {
 
             openingAccount(account, accounts, dinero);
         } else if (choice == 'D'){
-            System.out.println("Changin password");
+            cls();
+            System.out.println("Changing password");
             System.out.println("What do you want to replace your password? ");
 
             String answer;
@@ -182,15 +197,24 @@ public class Main {
 
             openingAccount(account, accounts, dinero);
         } else if (choice == 'E'){
+            cls();
             System.out.println("Loging out from account...");
         } else {
+            cls();
             System.out.println("Invalid input");
             openingAccount(account, accounts, dinero);
         }
         
 
-        // for (String i : dinero.keySet()){
-            // System.out.println(i);
-        // }
+    }
+
+    public static void cls(){
+        try
+        {
+            new ProcessBuilder("cmd", "/c","cls").inheritIO().start().waitFor();
+        }catch(Exception E)
+            {
+                System.out.println(E);
+            }
     }
 }
